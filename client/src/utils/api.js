52bc29561f17d61a99406de3db05517a5c3a6224
mohammadAyan1 <mohammadayan2210@gmail.com>
2026-03-23 17:56:02 +0530

    ENV File Remove

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // ✅ Axios instance create
 const api = axios.create({
-    baseURL: `${import.meta.VITE_BACKEND_MAIN_URL}/api`, // backend URL
+    baseURL: `${import.meta.env.VITE_BACKEND_MAIN_URL}/api`, // backend URL
     withCredentials: true, // 🔥 important (cookies ke liye)
     headers: {
         "Content-Type": "application/json"
